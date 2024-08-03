12e827f50b91d0cc13676699ed07b3b8c7fac1a9
winniehhy <yoyowinnie3@gmail.com>
2024-08-03 16:14:54 +0800

    update for interface

--- src/ADT/QueueInterface.java
+++ src/ADT/QueueInterface.java
@@ -4,26 +4,29 @@ import java.util.NoSuchElementException;
 /**
  * An interface for the Queue ADT.
  * 
- * @param <T> The type of data that the queue stores.
+ * @param <E> type of elements that the queue will store
+ * 
+ * This interface defines the essential operations for a queue data structure,
+ * where elements are added to the back and removed from the front in a FIFO (First In, First Out) manner.
  * 
  * @author Heng Han Yee
  */
-public interface Queue<T> {
+public interface Queue<E> {
   
   /** Adds the given item to the back of the queue.
-   * @param item the item to add
+   * @param item to be added to the queue
    */
-  void enqueue(T item);
+  void enqueue(E item);
   
   /** Removes an item from the front of the queue and returns it.
    * @return the item at the front of the queue, or null if empty
    */
-  T dequeue();
+  E dequeue();
   
   /** Returns the item at the front of the queue, without removing it.
    * @return the item at the front of the queue, or null if empty
    */
-  T peek();
+  E peek();
   
   /** Returns true if the queue is empty.
    */
@@ -33,21 +36,21 @@ public interface Queue<T> {
   void clear();
   
   /** Inserts the specified element into the queue. Returns true if successful.
-   * @param item the item to add
+   * @param item to be added to the queue
    * @return true if the element was added successfully
    */
-  boolean offer(T item);
+  boolean offer(E item);
   
   /** Retrieves and removes the head of the queue, or returns null if the queue is empty.
    * @return the item at the front of the queue, or null if empty
    */
-  T poll();
+  E poll();
   
   /** Returns the element at the front of the queue. Throws an exception if the queue is empty.
    * @return the item at the front of the queue
    * @throws NoSuchElementException if the queue is empty
    */
-  T element();
+  E element();
   
   /** Converts the queue to an array.
    * @return an array containing all the items in the queue
@@ -55,5 +58,5 @@ public interface Queue<T> {
   Object[] toArray();
   
   /** Returns an iterator over the elements in the queue. */
-  Iterator<T> iterator();
+  Iterator<E> iterator();
 }
